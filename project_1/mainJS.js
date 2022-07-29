1473944df2fb179a2b9d4f3bf294b41ccc1242ae
Morty20007 <76158703+Serega8765@users.noreply.github.com>
2022-07-29 12:22:39 +0300

    Delete project_1 directory

--- project_1/mainJS.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.menu_open').click(function () {
-        $('.menu_ul').toggleClass('show_menu')
-    })
-    $('.menu_li').click(function () {
-        $('.menu_ul').toggleClass('show_menu')
-    })
-});

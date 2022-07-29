67832484850f1791d1a37c609915797cc310d312
Morty20007 <76158703+Serega8765@users.noreply.github.com>
2022-07-29 12:23:24 +0300

    Pr1

---
+++ mainJS.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.menu_open').click(function () {
+        $('.menu_ul').toggleClass('show_menu')
+    })
+    $('.menu_li').click(function () {
+        $('.menu_ul').toggleClass('show_menu')
+    })
+});

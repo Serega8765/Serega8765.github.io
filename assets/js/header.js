a1c69b3fdb9813d4298174618a189ca1610e1823
NeDolbaeb <gradik.st@gmail.com>
2023-04-23 00:13:08 +0300

    123

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     var eyeless = $('.btn--eyeless'),
         eyelessContent = $('.eyeless-content'),
-        liMain = $('.nav-li--main');
+        liMain = $('.nav-li--main'),
+        btnBurger = $('.header-burger_btn'),
+        nav = $('.nav--main'),
+        closeBurger = $('.close_menu');
 
     var isOpen = false;
 
@@ -16,13 +19,32 @@ $(document).ready(function () {
         }
     );
 
+    btnBurger.click(function (e) { 
+        nav.add(nav.find('ul')).stop(true, true);
+        nav.fadeIn(200).css('display', 'flex');
+        closeBurger.fadeIn(300);
+        nav.find('ul').animate({width : '300px'}, 300).css('overflow', 'visible');
+    });
+
+    closeBurger.add(nav).click(function (e) {
+        if ((e.target.className == 'nav--main') ||
+           (e.target.className == 'close_menu')){
+            e.stopPropagation();
+            nav.add(nav.find('ul')).stop(true, true);
+            closeBurger.fadeOut(200);
+            nav.find('ul').animate({width : '0px'}, 300, function () {nav.fadeOut(200);}).css('overflow', 'visible');    
+           }
+    });
+
     liMain.click(function(e){
         if(!isOpen){
             isOpen = true;
+            liMain.stop(true, true)
             $(this).children('ul').show(300);
             $(this).addClass('selected');
         } else{
             isOpen = false;
+            liMain.children('ul').stop(true, true)
             liMain.children('ul').hide(300);
             liMain.removeClass('selected');
         }
@@ -31,6 +53,7 @@ $(document).ready(function () {
     $(document).click(function (e){
         if(liMain.has(e.target).length === 0){
             isOpen = false;
+            liMain.children('ul').stop(true, true)
             liMain.children('ul').hide(300);
             liMain.removeClass('selected');
         };
--- assets/js/main-content.js
+++ assets/js/main-content.js
@@ -1,11 +1,147 @@
 $(document).ready(function () {
-  var cardImg = $('.content_card-card_img');
+  var cardImg = $('.content_card-card_img'),
+      newsBlock = $('.short_news-block'),
+      newsLine = $('.news-line'),
+      afishaText = $('.afisha-content--text'),
+      newsText = [$('#news1'), $('#news2'), $('#news3'), $('#news4')],
+      index = 1,
+      lenGen = 0,
+      idNewsLine = "",
+      txt = "";
 
 
-      cardImg.hover(function () {
-          cardImg.addClass('card_hover');
-        }, function () {
-          cardImg.removeClass('card_hover');
-        }
-      );
+  cardImg.hover(function () {
+      cardImg.addClass('card_hover');
+    }, function () {
+      cardImg.removeClass('card_hover');
+    }
+  );
+
+  function changeNews(index){
+    newsBlock.stop(true, true); 
+
+    idNewsLine = "#newsLine" + (index);
+    newsBlock.fadeOut(0);
+    $('#news' + index).fadeIn(1000);
+
+    newsLine.attr('src', 'assets/img/slider/sliderLine.svg');
+    $(idNewsLine).attr('src', 'assets/img/slider/sliderLineSelected.svg');
+  }
+
+  newsLine.click(function (e) {
+    if ($(this).attr('src') !== 'assets/img/slider/sliderLineSelected.svg'){
+      index = $(this).attr('id').slice(-1); 
+
+      changeNews(index);
+    }
+  });
+  if ($(window).width() < 551){
+    newsBlock.hide();
+    $('#news1').show();
+
+    $('.content-news').swipe({
+      swipeStatus:function(event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection){
+          if (phase=="end"){ 
+              if (direction == 'right') {
+                  if(index > 1){
+                      index--;
+                  }else{
+                      index = 4;
+                  }
+                  changeNews(index);
+              }
+              if (direction == 'left') {
+                  if(index < 4){
+                      index++;
+                  }else{
+                      index = 1;
+                  }
+                  changeNews(index);
+              }
+          }
+      }, triggerOnTouchEnd:false,
+      threshold:100 // сработает через 20 пикселей
+    });
+  };
+
+  function cutter(obj, len){
+    obj.find('p').each(function( index, element ) {
+      txt = $(this).text().split(" ");
+      txt = jQuery.grep(txt, function( a ) {
+        return a !== '';
+      });
+
+      if (lenGen + txt.length < len){
+        lenGen = lenGen + txt.length;
+        $(this).text(txt.join(' '));
+      } else {
+        txt = txt.slice(0, len - lenGen);
+        if (txt == 0){
+          $(this).css('display', 'none');
+        };
+        lenGen = lenGen + txt.length;
+        $(this).text(txt.join(' ') + '...');
+      }
+    });
+    lenGen = 0;
+  }
+
+  cutter(afishaText, 50);
+
+  if ($(window).width() < 1401){
+    cutter(afishaText, 45);
+  }
+
+  if ($(window).width() < 1001 && $(window).width() > 890){
+    cutter(afishaText, 35);
+    $.each(newsText, function (index, el){
+      cutter($(this), 25);
+    });
+  }
+
+  if ($(window).width() < 890  && $(window).width() > 769){
+    $.each(newsText, function (index, el){
+      cutter($(this), 20);
+    });
+  }
+
+  if ($(window).width() < 769 && $(window).width() > 731){
+    cutter(afishaText, 21);
+  }
+
+  if ($(window).width() < 731 && $(window).width() > 551){
+    cutter(afishaText, 35);
+    $.each(newsText, function (index, el){
+      cutter($(this), 15);
+    });
+  }
+
+  if ($(window).width() < 551){
+    $.each(newsText, function (index, el){
+      cutter($(this), 40);
+    });
+  }
+
+  if ($(window).width() < 481){
+    cutter(afishaText, 30);
+    $.each(newsText, function (index, el){
+      cutter($(this), 31);
+    });
+  }
+
+  if ($(window).width() < 456){
+    $.each(newsText, function (index, el){
+      cutter($(this), 25);
+    });
+  }
+
+  if ($(window).width() < 361){
+    cutter(afishaText, 25);
+    $.each(newsText, function (index, el){
+      cutter($(this), 20);
+    });
+  }
+  
+  
 });
+
